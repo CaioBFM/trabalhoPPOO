8048c6f9ba480e04eb976596dd12416466eb8167
Fabio Valim <oficialvalim@gmail.com>
2025-11-25 17:03:25 -0300

    fix: altera o tamanho da tela de visualizacao do simulador

--- src/Principal.java
+++ src/Principal.java
@@ -1,6 +1,6 @@
 public class Principal{
   public static void main(String[] args){
-    Simulator simulator = new Simulator();
+    Simulator simulator = new Simulator(150,200);
     simulator.runLongSimulation();
     //simulator.simulate(300);
   }
